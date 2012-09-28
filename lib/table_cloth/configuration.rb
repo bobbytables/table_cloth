70729e88f9c5d29d1193545d203d8f37d55dc4d1
Robert Ross <robert@creativequeries.com>
2012-09-28 12:07:05 -0700

    Add configuration + spec.

--- lib/table_cloth/configuration.rb
+++ lib/table_cloth/configuration.rb
@@ -1,9 +1,23 @@
 module TableCloth
   class Configuration
-    cattr_accessor :table_class
-    
-    def self.configure(&block)
-      yield
+    cattr_accessor :table
+    cattr_accessor :thead
+    cattr_accessor :th
+    cattr_accessor :tbody
+    cattr_accessor :tr
+    cattr_accessor :td
+
+    self.table = ActiveSupport::OrderedOptions.new
+    self.thead = ActiveSupport::OrderedOptions.new
+    self.th    = ActiveSupport::OrderedOptions.new
+    self.tbody = ActiveSupport::OrderedOptions.new
+    self.tr    = ActiveSupport::OrderedOptions.new
+    self.td    = ActiveSupport::OrderedOptions.new
+
+    class << self
+      def configure(&block)
+        block.arity > 0 ? block.call(self) : yield
+      end
     end
   end
 end
--- spec/lib/configuration_spec.rb
+++ spec/lib/configuration_spec.rb
@@ -3,8 +3,33 @@ require 'spec_helper'
 describe TableCloth::Configuration do
   subject { Class.new(TableCloth::Configuration) }
 
-  it 'configures default table classes' do
-    subject.table_class = 'table'
-    subject.table_class.should == 'table'
+  it 'configures table options' do
+    subject.table.classes = 'table'
+    subject.table.classes.should == 'table'
+  end
+
+  it 'configures thead options' do
+    subject.thead.classes = 'thead'
+    subject.thead.classes.should == 'thead'
+  end
+
+  it 'configures th options' do
+    subject.th.classes = 'th'
+    subject.th.classes.should == 'th'
+  end
+
+  it 'configures tbody options' do
+    subject.tbody.classes = 'tbody'
+    subject.tbody.classes.should == 'tbody'
+  end
+
+  it 'configures tr options' do
+    subject.tr.classes = 'tr'
+    subject.tr.classes.should == 'tr'
+  end
+
+  it 'configures td options' do
+    subject.td.classes = 'td'
+    subject.td.classes.should == 'td'
   end
 end
