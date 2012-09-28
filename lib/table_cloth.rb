47f711a6cf7b5ddf1735e8d7281dcb39106cf5a6
Robert Ross <robert@creativequeries.com>
2012-09-28 12:07:22 -0700

    Add autoload for action / action column.

--- lib/table_cloth.rb
+++ lib/table_cloth.rb
@@ -1,13 +1,20 @@
 require 'action_view'
 require 'table_cloth/version'
+require 'table_cloth/base'
 
 module TableCloth
   autoload :Configuration, 'table_cloth/configuration'
-  autoload :Base, 'table_cloth/base'
   autoload :Column, 'table_cloth/column'
+  autoload :Action, 'table_cloth/action'
   autoload :Presenter, 'table_cloth/presenter'
 
   module Presenters
     autoload :Default, 'table_cloth/presenters/default'
   end
+
+  module Columns
+    autoload :Action, 'table_cloth/columns/action'
+  end
 end
+
+TableCloth::Base.presenter ::TableCloth::Presenters::Default
