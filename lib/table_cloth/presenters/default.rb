bdc710d3e0dbf25e8dc9171ddd214c2072350b08
Yuri Kovalov <yuri@yurikoval.com>
2012-12-07 19:03:28 +0900

    Fix so that options are passed to each row

--- lib/table_cloth/presenters/default.rb
+++ lib/table_cloth/presenters/default.rb
@@ -20,7 +20,7 @@ module TableCloth
       end
 
       def render_td(column, object)
-        td_options = column.options.delete(:td_options) || {}
+        td_options = column.options[:td_options] || {}
         value = column.value(object, view_context, table)
 
         if value.is_a?(Array)
