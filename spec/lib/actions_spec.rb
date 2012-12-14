13afb98c035686c21bc102727734dc6dd5656638
Robert Ross <robert@creativequeries.com>
2012-12-14 01:37:07 -0800

    Cleanup actions spec.

--- spec/lib/actions_spec.rb
+++ spec/lib/actions_spec.rb
@@ -4,26 +4,25 @@ describe TableCloth::Actions do
   subject { TableCloth::Actions }
 
   context "block syntax" do
-
     it "accepts a block" do
       actions = subject.new do
         action { 'Edit' }
       end
 
-      actions.column.should have(1).actions
+      expect(actions.column).to have(1).actions
     end
 
     it "accepts options with a block" do
-      expect do
-        subject.new(class: "actions") do
-          action { "Edit" }
-        end
-      end.not_to raise_error
+      actions = subject.new(class: "actions") do
+        action { "Edit" }
+      end
+
+      expect(actions.options[:class]).to eq("actions")
     end
 
     it ".all returns all actions for the column" do
       actions = subject.new { action { "Edit" } }
-      actions.should have(1).all
+      expect(actions.all.length).to be 1
     end
   end
 end
