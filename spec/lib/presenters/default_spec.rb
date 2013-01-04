46f9f3b30e1e8334d8834b2b4c496e1389d618ee
Robert Ross <robert@creativequeries.com>
2013-01-04 12:16:30 -0800

    Remove config specs for default presenter, irrelevant since we're testing wrapper tag from presenter now.

--- spec/lib/presenters/default_spec.rb
+++ spec/lib/presenters/default_spec.rb
@@ -60,41 +60,6 @@ describe TableCloth::Presenters::Default do
     end
   end
 
-  context 'configuration' do
-    let(:doc) { Nokogiri::HTML(subject.render_table) }
-
-    it 'tables have a class attached' do
-      TableCloth.stub config_for: {class: "table"}
-      doc.at_xpath('//table')[:class].should include 'table'
-    end
-
-    it 'thead has a class attached' do
-      TableCloth.stub config_for: {class: "thead"}
-      doc.at_xpath('//thead')[:class].should include 'thead'
-    end
-
-    it 'th has a class attached' do
-      TableCloth.stub config_for: {class: "th"}
-      doc.at_xpath('//th')[:class].should include 'th'
-    end
-
-    it 'tbody has a class attached' do
-      TableCloth.stub config_for: {class: "tbody"}
-      doc.at_xpath('//tbody')[:class].should include 'tbody'
-    end
-
-    it 'tr has a class attached' do
-      TableCloth.stub config_for: {class: "tr"}
-      doc.at_xpath('//tr')[:class].should include 'tr'
-    end
-
-    it 'td has a class attached' do
-      TableCloth.stub config_for: {class: "td"}
-      doc.at_xpath('//td')[:class].should include 'td'
-    end
-
-  end
-
   context "column configuration" do
     let(:column) { FactoryGirl.build(:column, options: {td_options: {class: "email_column"}}) }
     let(:model) { FactoryGirl.build(:dummy_model) }
@@ -115,45 +80,6 @@ describe TableCloth::Presenters::Default do
         expect(td.text).to include "robert@example.com"
         expect(td[:class]).to eq("special-class")
       end
-    end
-  end
-
-  context "table configuration" do
-    let(:doc) { Nokogiri::HTML(subject.render_table) }
-    let(:dummy_table) do
-      Class.new(TableCloth::Base) do
-        column :email
-
-        config.table.class = "table2"
-        config.thead.class = "thead2"
-        config.th.class    = "th2"
-        config.tbody.class = "tbody2"
-        config.tr.class    = "tr2"
-        config.td.class    = "td2"
-      end
-    end
-
-    include_examples "table configuration"
-
-    context "is extendable" do
-      let(:parent_table) do
-        Class.new(TableCloth::Base) do
-          column :email
-
-          config.table.class = "table2"
-          config.thead.class = "thead2"
-          config.th.class    = "th2"
-          config.tbody.class = "tbody2"
-          config.tr.class    = "tr2"
-          config.td.class    = "td2"
-        end
-      end
-
-      let(:dummy_table) do
-        Class.new(parent_table)
-      end
-      
-      include_examples "table configuration"
     end
   end
 end
